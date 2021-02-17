4a9ec0e1eb856c5f05bcf1cdf049599602ae3ece
Anush Pallaev <77150208+Nushko@users.noreply.github.com>
2021-02-17 22:06:38 +0300

    Исправил таймер

---
+++ lesson11B_13.02.21/lesson11B_13.02.21/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO.Pipes;
+using System.Text;
+
+namespace lesson11B_13._02._21
+{
+    public class Client
+    {
+        
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+
+        public static List<Client> clientbase = new List<Client>();
+        public static List<Client> checkbalancebase = new List<Client>();
+    }
+}
